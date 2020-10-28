82569d76afbd8c367dce464eb908718ba0ec36bd
Paige Young <10886160@uvu.edu>
2020-10-28 15:04:49 -0600

    male button changes

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -1,8 +1,10 @@
 import { people } from '../data/people.js'
 
+const mainContent = document.querySelector('#main')
+
 const mainHeader = document.createElement('header')
 mainHeader.className = 'mainHeader'
-document.body.appendChild(mainHeader)
+document.body.insertBefore(mainHeader, mainContent)
 
 const maleButton = document.createElement('button')
 maleButton.textContent = 'Male Characters'
@@ -16,11 +18,29 @@ const otherButton = document.createElement('button')
 otherButton.textContent = 'Other Characters'
 mainHeader.appendChild(otherButton)
 
+
 const maleCharacters = people.filter(person => person.gender === 'male')
 console.log(maleCharacters)
 
 maleButton.addEventListener('click', event => {
+    maleCharacters.forEach((element) => {
     const charFigure = document.createElement('figure')
     const charImg = document.createElement('img')
-    const charFigure = document.createElement('figcaption')
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = 'Luke Skywalker'
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+
+    mainContent.appendChild(charFigure)
+    })
 })
+
+let theUrl = "https://swapi.co/api/people/2/"
+
+function getLastNumber(url) {
+    console.log(url) 
+}
+
+getLastNumber(theUrl)
